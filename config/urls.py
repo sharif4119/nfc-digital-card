d99e6f4bbdeadbb0e88fef1a5cdc835f506859ea
ar210038 <al-rafi210038@diit.edu.bd>
2026-09-20 07:30:18 +0600

    Fix media serving on Render

--- config/urls.py
+++ config/urls.py
@@ -1,36 +1,37 @@
-from django.contrib import admin
-from django.urls import include, path
 from django.conf import settings
-from django.conf.urls.static import static
-
+from django.contrib import admin
+from django.urls import include, path, re_path
+from django.views.static import serve
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("core.urls")),
 
+    path("", include("core.urls")),
     path("", include("accounts.urls")),
 
     path(
         "accounts/",
         include("django.contrib.auth.urls"),
     ),
+
     path(
-    "profile/",
-    include("profiles.urls"),
+        "profile/",
+        include("profiles.urls"),
     ),
+
     path("", include("cards.urls")),
     path("", include("products.urls")),
     path("", include("orders.urls")),
-    
-    path(
-    "api/seller/",
-    include("seller_api.urls"),
-),
-    
-]
 
-urlpatterns += static(
-    settings.MEDIA_URL,
-    document_root=settings.MEDIA_ROOT,
-)
+    path(
+        "api/seller/",
+        include("seller_api.urls"),
+    ),
+
+    re_path(
+        r"^media/(?P<path>.*)$",
+        serve,
+        {"document_root": settings.MEDIA_ROOT},
+    ),
+]
